832acc49a045aafc850ab82bc4dac4e336440204
tomharris <tomharris@idtdna.com>
2017-11-19 12:35:01 -0600

    Added BindingHelper to remove extraneous code;

---
+++ src/TravelingSalesPersonVisualizer/Utilities/BindingHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace TravelingSalesPersonVisualizer.Utilities
+{
+    public static class BindingHelper
+    {
+        public static void BindTextBox(DependencyObject dependencyObject, object dataSource, string propertyName)
+        {
+            BindingOperations.SetBinding(dependencyObject, TextBox.TextProperty,
+                                         new Binding
+                                             {
+                                                 Source = dataSource,
+                                                 Path = new PropertyPath(propertyName),
+                                                 Mode = BindingMode.TwoWay,
+                                                 UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
+                                             });
+        }
+    }
+}
